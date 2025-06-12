a20b76a09caea307851a72053c10b8cfd030be16
Zartasha Ansari <zartashaansari766@gmail.com>
2025-06-12 23:41:00 +0500

    Add files via upload

---
+++ brandname_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Brand Name Generator")
+city=input("Whats the name of the city you grew up in?\n")
+pet=input("Whats the name of your first pet?\n")
+print("Your brand name could be "+ city +" "+ pet)
